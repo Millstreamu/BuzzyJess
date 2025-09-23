28ed7522c69894055211ef151108b6a54f481682
Millstreamu <j.winson23@hotmail.com>
2025-09-23 16:20:50 +1000

    Switch queen and brood menus to radial

--- scripts/controllers/QueenController.gd
+++ scripts/controllers/QueenController.gd
@@ -3,20 +3,20 @@ class_name QueenController
 
 @export var panel_path: NodePath
 
-var panel: QueenFeedPanel
+var panel: QueenFeedRadialMenu
 
 func _ready() -> void:
     if panel_path != NodePath():
         panel = get_node_or_null(panel_path)
     if panel:
-        panel.panel_closed.connect(_on_panel_closed)
+        panel.menu_closed.connect(_on_panel_closed)
 
 func is_menu_open() -> bool:
     return panel != null and panel.is_open()
 
-func open_panel() -> void:
+func open_panel(world_position: Vector2) -> void:
     if panel:
-        panel.open()
+        panel.open_at(world_position)
 
 func close_menu() -> void:
     if panel and panel.is_open():
